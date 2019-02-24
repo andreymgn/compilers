c33817ad17759f7424d4061ad778511d643eaafe
Andrey Ivanov <andreymgn@protonmail.ch>
2019-02-24 18:55:10 +0300

    Implement DFA minimization

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from parser import infixToPostfix, tokenize, syntaxTreeFromRPN
+from DFA import DFA
 
 # regex = '(0|1(01*0)*1)*'
-regex = '(01*1)*1'
-# regex = "(a|b)*abb"
+# regex = '(01*1)*1'
+regex = "(a|b)*abb"
 # regex = "(a|b)*"
 # regex = "(a*|b*)*"
 sigma = set(regex) - set('()|*')
@@ -19,14 +20,57 @@ followpos = st.getFollowpos()
 print(followpos)
 
 dfa = st.toDFA(followpos, positions, sigma)
-print(dfa)
 
-print(dfa.accepts('0'))
-print(dfa.accepts('011'))
-print(dfa.accepts('110'))
-print(dfa.accepts('1001'))
-print(dfa.accepts('1100'))
-print(dfa.accepts('1111'))
-print(dfa.accepts('100010'))
+# tt = {
+#     frozenset(['a']): {
+#         '0': frozenset(['b']),
+#         '1': frozenset(['c'])
+#     },
+#     frozenset(['b']): {
+#         '0': frozenset(['a']),
+#         '1': frozenset(['d'])
+#     },
+#     frozenset(['c']): {
+#         '0': frozenset(['e']),
+#         '1': frozenset(['f'])
+#     },
+#     frozenset(['d']): {
+#         '0': frozenset(['e']),
+#         '1': frozenset(['f'])
+#     },
+#     frozenset(['e']): {
+#         '0': frozenset(['e']),
+#         '1': frozenset(['f'])
+#     },
+#     frozenset(['f']): {
+#         '0': frozenset(['f']),
+#         '1': frozenset(['f'])
+#     }
+# }
+
+# start = 'a'
+
+# end = set([frozenset(['c']), frozenset(['d']), frozenset(['e'])])
+
+# dfa = DFA(tt, start, end)
+
+# print(dfa.accepts('0'))
+# print(dfa.accepts('011'))
+# print(dfa.accepts('110'))
+# print(dfa.accepts('1001'))
+# print(dfa.accepts('1100'))
+# print(dfa.accepts('1111'))
+# print(dfa.accepts('100010'))
+
+#dfa.visualize()
+
+print('a')
+eqCls = dfa.findEquivalenceClasses(sigma)
+for p in eqCls:
+    print(p)
+
+dfa.mergeEquivalentStates(eqCls)
+
+print(dfa.ends)
 
 dfa.visualize()
